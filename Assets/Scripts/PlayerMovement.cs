4c5975a84b8ea429f4a11d62a97c660662c2de6f
Peter Kang <pkang@berkeley.edu>
2020-10-14 15:47:57 -0700

    implemented basic character movement using placeholder sprites

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    #region Component Variables
+    public Rigidbody2D rb;
+    public Animator animator;
+    #endregion
+
+    #region Movement Variables
+    public float moveSpeed = 5f;
+    Vector2 movement;
+    #endregion
+
+    #region Movement Functions
+    // Moves the player's rigidbody based on our movement variables
+    private void Move()
+    {
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+    #endregion
+
+    #region Unity Functions
+    // Update is called once per frame
+    void Update()
+    {
+        // Get Input
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        // Set Animations
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Speed", movement.sqrMagnitude);
+    }
+
+    // FixedUpdate is called on a fixed timer
+    private void FixedUpdate()
+    {
+        // Execute Player Movement
+        Move();
+    }
+    #endregion
+}
